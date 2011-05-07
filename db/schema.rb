e1b461c32d14444b03f002662599c6a5f4f09210
Richard Huang <flyerhzm@gmail.com>
2011-05-07 10:47:50 +0800

    client application model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,34 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110507021251) do
+ActiveRecord::Schema.define(:version => 20110507024610) do
+
+  create_table "client_applications", :force => true do |t|
+    t.string   "name"
+    t.string   "key"
+    t.string   "secret"
+    t.integer  "developer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "developers", :force => true do |t|
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "developers", ["email"], :name => "index_developers_on_email", :unique => true
+  add_index "developers", ["reset_password_token"], :name => "index_developers_on_reset_password_token", :unique => true
 
   create_table "parents", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
