8adc85b7a81f366b9673d6a116359cea98dc04a5
Richard Huang <flyerhzm@gmail.com>
2011-05-07 09:37:14 +0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'mysql2', '0.2.7'
+gem 'capistrano'
+gem 'haml'
+gem 'devise'
+gem 'simple_form'
+
+group :development do
+  gem 'thin'
+  gem 'awesome_print', :require => 'ap'
+end
+
+group :test do
+  gem 'ZenTest'
+  gem 'autotest'
+  gem 'autotest-rails'
+  gem 'spork', '0.9.0.rc.7'
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'remarkable_activerecord', '4.0.0.alpha4'
+  gem 'watchr'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = '17b20b1435e0813cf398c703de42e81287a4dc80ef49c091f8466c56be4dd43ea19130524bdb835009256a813db3c8c6ec58b57f9142d3b2d886d044d01123fb'
