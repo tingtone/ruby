3d1dabef4f272da96fe0f556bac16c02e7b565e4
Richard Huang <flyerhzm@gmail.com>
2011-05-07 20:26:07 +0800

    update client api example

---
+++ examples/api_test.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'net/http'
+require 'uri'
+require 'cgi'
+require 'base64'
+require 'hmac'
+require 'hmac-sha1'
+
+key = "Uj0WwBCn9Rmb3yNofvRfJQ"
+secret = "VV1Fqowyw9AzQyyAi3wMzKbocUGnpuFrUIT0SSsEZU"
+
+string = "/api/v1/client_applications/1.json+#{secret}+GET+key=#{key}&param1=value1&param2=value2"
+salt = CGI.escape(secret).gsub("%7E", '~').gsub("+", "%20")
+signature = Base64.encode64(HMAC::SHA1.digest(salt, string)).chomp.gsub(/\n/, '')
+
+uri = URI.parse("http://ec2-50-16-134-27.compute-1.amazonaws.com/api/v1/client_applications/1.json?key=#{key}&param1=value1&param2=value2&signature=#{signature}")
+http = Net::HTTP.new(uri.host, uri.port)
+
+response = http.request_get(uri.request_uri)
+puts response.body
