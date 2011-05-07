8746d73d1c521fe547c619f9132ccd3bd9789551
Richard Huang <flyerhzm@gmail.com>
2011-05-07 19:47:38 +0800

    don't hack http header for api authentication

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -15,20 +15,21 @@ class Api::BaseController < ApplicationController
     end
 
     def has_valid_signature?
-      if (key = env['HTTP_X_OF_KEY']) && (game = ClientApplication.find_by_key(key)) && (http_signature = env['HTTP_X_OF_SIGNATURE'])
+      if (key = params['key']) && (game = ClientApplication.find_by_key(key)) && (signature = params.delete('signature'))
         raw_params = if (request.get? || request.delete?)
           request.query_string
         else
           raw_post
         end
+        raw_params.sub!(/&signature=.*$/, '')
         string = "#{request.path}+#{game.secret}+#{request.request_method.to_s.upcase}+#{raw_params}"
-        signature(string, game.secret) == http_signature
+        sign(string, game.secret) == signature
       else
         false
       end
     end
 
-    def signature(string, secret)
+    def sign(string, secret)
       salt = "#{escape(secret)}"
       Base64.encode64(HMAC::SHA1.digest(salt, string)).chomp.gsub(/\n/,'')
     end
