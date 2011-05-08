6c085795ce9b378b6dd7c2cdd6e0dcf129e1424e
Richard Huang <flyerhzm@gmail.com>
2011-05-08 21:06:17 +0800

    update api_test

--- examples/api_test.rb
+++ examples/api_test.rb
@@ -9,11 +9,11 @@ require 'hmac-sha1'
 key = "Uj0WwBCn9Rmb3yNofvRfJQ"
 secret = "VV1Fqowyw9AzQyyAi3wMzKbocUGnpuFrUIT0SSsEZU"
 
-string = "/api/v1/client_applications/1.json+#{secret}+GET+key=#{key}&param1=value1&param2=value2"
+string = "/api/v1/children.json+#{secret}+GET+key=#{key}&parent_id=1"
 salt = CGI.escape(secret).gsub("%7E", '~').gsub("+", "%20")
 signature = Base64.encode64(HMAC::SHA1.digest(salt, string)).chomp.gsub(/\n/, '')
 
-uri = URI.parse("http://ec2-50-16-134-27.compute-1.amazonaws.com/api/v1/client_applications/1.json?key=#{key}&param1=value1&param2=value2&signature=#{signature}")
+uri = URI.parse("http://ec2-50-16-134-27.compute-1.amazonaws.com/api/v1/children.json?key=#{key}&parent_id=1&signature=#{signature}")
 http = Net::HTTP.new(uri.host, uri.port)
 
 response = http.request_get(uri.request_uri)
