42ab07dc0fe895197fcbbadba08d513f718f1cbc
AlexZhang <blackanger.z@gmail.com>
2011-06-10 17:15:20 +0800

    forums controller test pass

--- spec/controllers/forum/forums_controller_spec.rb
+++ spec/controllers/forum/forums_controller_spec.rb
@@ -24,18 +24,18 @@ describe Forum::ForumsController  do
         post :create, :forum => { :name => 'Games', :description => 'Talk about something about games' }
         response.response_code.should == 302
         @forum = assigns[:forum]
-        @forum.permalink.should == "gamesw"
+        @forum.permalink.should == "games"
         @forum.position.should == 1
         @forum.description_html.should == "<p>Talk about something about games</p>"
         @forum.state.should == "public"
-        
       end
       
       it "should can be update a forum" do
-        put :update, :forum =>{ :name => "Apps"}, :id => "games"
-
+        forum = Factory(:forum)
+        put :update, :forum =>{ :name => "Apps"}, :id => forum.permalink
         @forum = assigns[:forum]
-        @forum.permalink.should == "apps"
+        @forum.name == "Apps"
+        @forum.permalink.should == "control-room"
         @forum.position.should  == 1
       end
       
