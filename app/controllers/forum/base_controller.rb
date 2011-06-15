b7e485632add2217084eca420337d92aab5bf2af
AlexZhang <blackanger.z@gmail.com>
2011-06-15 17:11:41 +0800

    modify

--- app/controllers/forum/base_controller.rb
+++ app/controllers/forum/base_controller.rb
@@ -1,19 +1,17 @@
 class Forum::BaseController < ApplicationController
   include Forum::BaseHelper
-  before_filter :current_user
-  
   layout 'forum'
-  
-  
-  
-  # == Display a flash if CanCan doesn't allow access
-  rescue_from CanCan::AccessDenied do |exception|
-    flash[:alert] = "Access denied!"
-    redirect_to forum_root_url
-  end
 
-  def current_user
-    @current_user = current_forum_user if forum_user_signed_in?
-  end
+  protected
+  
+    # == Display a flash if CanCan doesn't allow access
+    rescue_from CanCan::AccessDenied do |exception|
+      flash[:alert] = "Access denied!"
+      redirect_to forum_root_url
+    end
+
+    def current_user
+      current_forum_user if forum_user_signed_in?
+    end
 
 end
