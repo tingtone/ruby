7f8c58e8cc808b0a3e62f936ec9d0d7613b50195
AlexZhang <blackanger.z@gmail.com>
2011-06-23 16:59:01 +0800

    modify logger puts

--- app/models/permissions/control_center.rb
+++ app/models/permissions/control_center.rb
@@ -13,14 +13,19 @@ class Permissions::ContorlCenter
 
     def dispatch(current_ability, forum_user)
       if forum_user.has_role? :admin
+        Rails.logger.debug("dispatch---------> #{admin}")
         Permissions::AdminPerm.got_permissions current_ability
       elsif forum_user.has_role? :parent
+        Rails.logger.debug("dispatch---------> #{parent}")
         Permissions::ParentPerm.got_permissions current_ability
       elsif forum_user.has_role? :developer
+        Rails.logger.debug("dispatch---------> #{developer}")
         Permissions::DeveloperPerm.got_permissions current_ability
       elsif forum_user.has_role? :guest
+        Rails.logger.debug("dispatch---------> #{guest}")
         Permissions::GuestPerm.got_permissions current_ability
       else
+        Rails.logger.debug("dispatch---------> #{anybody}")
         current_ability.can :read, [Forum,Topic, Post]
         current_ability.can :about, Forum
         current_ability.can :news,  Forum
