6595fe33af7891ba1e06581240d177513dfe4969
AlexZhang <blackanger.z@gmail.com>
2011-06-24 00:10:50 +0800

    modify

--- app/models/forum_user.rb
+++ app/models/forum_user.rb
@@ -31,7 +31,7 @@ class ForumUser
   references_many :topics
   references_many :posts
   
-  references_and_referenced_in_many :roles, autosave: true
+  references_and_referenced_in_many :roles, :validate => false
   
   # Setting Moderator for Forum
   references_and_referenced_in_many :forums
