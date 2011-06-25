b82ec5dfff5d1fe4cd6f701a29b8e19a3c5707dc
Richard Huang <flyerhzm@gmail.com>
2011-06-25 18:38:52 +0800

    use deep_merge

--- app/controllers/api/v1/parent_sessions_controller.rb
+++ app/controllers/api/v1/parent_sessions_controller.rb
@@ -16,23 +16,42 @@ class Api::V1::ParentSessionsController < Api::V1::BaseController
       parent.add_device(params[:device_identifier])
       result = {
         :error => false,
-        :parent => {},
         :time_summary => parent.children.collect { |child| current_client_application.time_summary(child).merge(:child_id => child.id) }
       }
       if params[:timestamp].blank? || params[:timestamp].to_i < parent.updated_at.to_i
-        result[:parent][:id] = parent.id
-        result[:parent][:authentication_token] = parent.authentication_token
-        result[:parent][:global_rule_definitions] = RuleDefinition.globals
+        result.deep_merge!({
+          :parent => {
+            :id => parent.id,
+            :authentication_token => parent.authentication_token,
+            :global_rule_definitions => RuleDefinition.globals
+          }
+        })
         if !params[:email] || !params[:password]
-          result[:parent][:email] = parent.email
-          result[:parent][:client_encrypted_password] = parent.client_encrypted_password
+          result.deep_merge!({
+            :parent => {
+              :email => parent.email,
+              :client_encrypted_password => parent.client_encrypted_password
+            }
+          })
         end
       end
-      result[:parent][:children] = parent.children if params[:timestamp].blank? || params[:timestamp].to_i < parent.children_updated_at.to_i
-      if params[:timestamp].blank? || params[:timestamp].to_i < parent.rule_definitions_updated_at.to_i
-        result[:parent][:rule_definitions] = parent.children.collect { |child| RuleDefinition.for_child_client_application(child, current_client_application).merge(:child_id => child.id) }
+      if params[:timestamp].blank? || params[:timestamp].to_i < parent.children_updated_at.to_i
+        result.deep_merge!({
+          :parent => {
+            :children => parent.children
+          }
+        })
       end
-      result[:bonus] = parent.children.collect { |child| child.bonus }.flatten if params[:timestamp].blank? || params[:timestamp].to_i < parent.bonus_updated_at.to_i
+      if params[:timestamp].blank? || params[:timestamp].to_i < parent.rule_definitions_updated_at.to_i
+        result.deep_merge!({
+          :parent => {
+            :rule_definitions => parent.children.collect { |child| RuleDefinition.for_child_client_application(child, current_client_application).merge(:child_id => child.id) }
+          }
+        })
+      end
+      if params[:timestamp].blank? || params[:timestamp].to_i < parent.bonus_updated_at.to_i
+        result[:bonus] = parent.children.collect { |child| child.bonus }.flatten
+      end
       render :json => result
     else
       render :json => {:error => true}
