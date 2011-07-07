496629741650374e5eab4a00ca63501bfb29ae48
AlexZhang <blackanger.z@gmail.com>
2011-07-07 16:40:56 +0800

    add migrations

--- db/migrate/20110707082053_create_players.rb
+++ db/migrate/20110707082053_create_players.rb
@@ -1,7 +1,15 @@
 class CreatePlayers < ActiveRecord::Migration
   def self.up
     create_table :players do |t|
-
+      t.integer :user_id, :null => false
+      t.string  :device_identifier
+      t.string  :device_user_agent
+      t.string  :language
+      
+      t.string  :name
+      t.string  :gender
+      t.datetime  :birthday
+      
       t.timestamps
     end
   end
