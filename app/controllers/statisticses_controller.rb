2cede3f3b61c344763c4d689524154876cc55a87
AlexZhang <blackanger.z@gmail.com>
2011-07-08 00:57:35 +0800

    add annotation

--- app/controllers/statisticses_controller.rb
+++ app/controllers/statisticses_controller.rb
@@ -1,6 +1,7 @@
 class StatisticsesController < ApplicationController
   
   def index
+    #test data
     @h = HighChart.new('graph') do |f|
        f.series(:name=>'John', :data=>[3, 20, 3, 5, 4, 10, 12 ,3, 5,6,7,7,80,9,9])
        f.series(:name=>'Jane', :data=> [1, 3, 4, 3, 3, 5, 4,-46,7,8,8,9,9,0,0,9] )
