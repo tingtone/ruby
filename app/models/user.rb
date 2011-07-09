b6f3f424ce2a933c636ccdeed69316394e304d96
AlexZhang <blackanger.z@gmail.com>
2011-07-09 21:17:12 +0800

    name or email to login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,18 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :login
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :login
+  
   
   validates :name,  :presence => true, :uniqueness => true, :length => { :maximum => 100 }
   validates :email, :presence => true, :uniqueness => true
+  
+  protected
+    def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      login = conditions.delete(:login)
+      where(conditions).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    end
 end
