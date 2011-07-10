5108e7fca1d9d686813a84aafc009c629ae828f5
AlexZhang <blackanger.z@gmail.com>
2011-07-10 15:06:46 +0800

    modify

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,4 +1,6 @@
 class App < ActiveRecord::Base
+  include OAuth::Helper
+  
   has_many :player_apps
   has_many :players, :through => :player_apps
   
@@ -7,6 +9,13 @@ class App < ActiveRecord::Base
   
   has_many :time_trackers
   has_many :score_trackers
+
+  before_create :generate_keys
+  validates :name,  :presence => true, :uniqueness => true, :length => { :maximum => 100 }
+  validates :description, :presence => true
+  validates :screenshot,  :presence => true, :unless => :new_record?
+  validates :app_store_url,  :presence => true, :unless => :new_record?
+
 
   has_attached_file :screenshot, :styles => {:default => "100x100>"}
   has_attached_file :icon, :styles => {:default => "156x156>"}
@@ -21,4 +30,10 @@ class App < ActiveRecord::Base
   def full_icon_url(style_name)
     "#{RAILS_HOST}/public#{icon.url(style_name)}"
   end
+  
+  protected
+    def generate_keys
+      self.key = generate_key(16)
+      self.secret = generate_key(32)
+    end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,5 @@ Server::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
 
   config.action_mailer.default_url_options = {:host => 'localhost:3000'}
-  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
 
