bf47d7612fe76f319ddd83c3df08af7b41a9cb93
AlexZhang <blackanger.z@gmail.com>
2011-07-12 16:35:52 +0800

    modify change download apps bug

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -12,26 +12,30 @@ class Developer < User
   def exchange_all?
     self.exchange_app == 2
   end
-  
+
   def self.got_exchange_apps(developer_id, device)
-    @developer = Developer.find developer_id
-    @player = Player.find_by_device_identifier(device)
-    @installed_app = @player.apps
-    if @developer.exchange_own?
-      if @player.device_user_agent.match(/iPad/)
-        @apps = (@developer.apps - @installed_app).first(7)
-      elsif @player.device_user_agent.match(/iPhone/)
-        @apps = (@developer.apps - @installed_app).first(5)
-      end
-    elsif @developer.exchange_all?
-      if @player.device_user_agent.match(/iPad/)
-        @apps = (App.all - @installed_app).sample(7)
-      elsif @player.device_user_agent.match(/iPhone/)
-        @apps = (App.all - @installed_app).sample(5)
-      end
-    else
-      @apps = []
-    end #if
+    begin
+      @developer = Developer.find developer_id
+      @player = Player.find_by_device_identifier(device)
+      @installed_app = @player.apps
+      if @developer.exchange_own?
+        if @player.device_user_agent.match(/iPad/)
+          @apps = (@developer.apps - @installed_app).first(10)
+        elsif @player.device_user_agent.match(/iPhone/)
+          @apps = (@developer.apps - @installed_app).first(10)
+        end
+      elsif @developer.exchange_all?
+        if @player.device_user_agent.match(/iPad/)
+          @apps = (App.all - @installed_app).sample(10)
+        elsif @player.device_user_agent.match(/iPhone/)
+          @apps = (App.all - @installed_app).sample(10)
+        end
+      else
+        @apps = []
+      end #if
+    rescue
+      @apps = App.all.sample(10)
+    end
     return @apps
   end #self.got_exchange_apps
 end
