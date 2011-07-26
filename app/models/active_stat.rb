074c32bc597d4efa3b9e4c69f7c5e52d071086f0
AlexZhang <blackanger.z@gmail.com>
2011-07-26 18:48:10 +0800

    modify

--- app/models/active_stat.rb
+++ app/models/active_stat.rb
@@ -1,3 +1,31 @@
 class ActiveStat < ActiveRecord::Base
-  
+  def self.active_amount(current_developer)
+    # HighChart.new('graph') do |f|
+    #       yesterday = Date.yesterday.to_s(:db)
+    #       
+    #       active_stats = ActiveStat.where("DATE_FORMAT(current_day, '%Y-%m-%d') = ? and user_id = ?", yesterday, current_developer.id)
+    #       
+    #       datas = Array.new.tap do |arr|
+    # 
+    #           active_stats.each do |item|
+    #             arr << {name: item.current_day, active_amounts: item.active_amount}
+    #           end
+    # 
+    #       end
+      
+      
+      # datas = [{name: 'app1', active_amounts: [0, 0, 0,1, 1, 0, 0]}, {name: 'app2', active_amounts: [1, 0, 2,1, 1, 0, 0]}]
+      
+      # f.options[:chart][:defaultSeriesType] = 'column'
+      #      f.options[:title][:text]              = 'The number of people who install your apps 7 days prior to today.'
+      #      f.options[:x_axis][:categories]       = dates
+      #      f.options[:x_axis][:labels]           = { :align=>'center' }
+      #      f.options[:y_axis][:title][:text]     = 'numbers'
+      #      
+      #      datas.each do |item|
+      #        f.series(name: item[:name], data: item[:active_amounts])
+      #      end
+      
+    # end
+  end #self.active_amount
 end
--- app/models/gender_stat.rb
+++ app/models/gender_stat.rb
@@ -1,3 +1,21 @@
 class GenderStat < ActiveRecord::Base
-  
+  def self.most_gender(current_developer)
+    HighChart.new('graph') do |f|
+      yesterday = Date.yesterday - 1
+      gender_stats = GenderStat.where("DATE_FORMAT(current_day, '%Y-%m-%d') = ? and user_id = ?", yesterday, current_developer.id)
+      
+      f.options[:title][:text]    = 'The gender ratio of people using your application at Yesterday'
+      f.options[:subtitle][:text] = 'Inner circle: Girl, outer circle: Boy'
+      boy_datas, girl_datas = [], []
+      
+      gender_stats.each do |item|
+        boy_datas << {name: item.app_name, y: item.boy_amount}
+        girl_datas << {name: item.app_name, y: item.girl_amount}
+      end
+      
+      f.series(type: 'pie', name: 'boy', size: '45%', innerSize: '20%', data: boy_datas )
+      f.series(type: 'pie', name: 'girl', innerSize: '45%', data: girl_datas)
+    end
+
+  end #self.most_gender(current_developer)
 end
