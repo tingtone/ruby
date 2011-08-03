66a6e5ea2f8f10aff53f3cf899a214809be59801
AlexZhang <blackanger.z@gmail.com>
2011-08-03 19:53:36 +0800

    modify

--- app/models/install_apps_day_tracker.rb
+++ app/models/install_apps_day_tracker.rb
@@ -1,7 +1,7 @@
 class InstallAppsDayTracker < ActiveRecord::Base
   def self.apps_time(player_id)
-    yestoday = Date.today - 1
-    data = where("DATE_FORMAT(updated_at, '%Y-%m-%d') = ?", yestoday.to_s(:db)).order("total_time desc")
+    yesterday = Date.today - 1
+    data = where("DATE_FORMAT(updated_at, '%Y-%m-%d') = ? and player_id =?", yesterday.to_s(:db), player_id).order("total_time desc")
     
     app_ids_times = Hash.new.tap do |h|
       data.each{|d| h[d.app_id] = d.total_time}
